5fee01b8e3610069602ea68ba68a172c9ea464ff
Irwan Fathurrahman <meomancer@gmail.com>
2025-07-24 12:31:32 +0700

    Theming amsterdam

--- configs.py
+++ configs.py
@@ -1,59 +1,67 @@
 # -*- coding: utf-8 -*-
 home_images_default = [
+    # {
+    #     'image': 'alex-siale.jpg',
+    #     'main_color': '#fff',
+    #     'main_title_color': '#fff',
+    #     'author': 'Alez Siale',
+    #     'author_url': 'https://unsplash.com/@alexsialecreative',
+    #     'subtitle_color': '#fff'
+    # },
+    # {
+    #     'image': 'anton-repponen.jpg',
+    #     'main_color': '#fff',
+    #     'main_title_color': '#fff',
+    #     'author': 'Anton Repponen',
+    #     'author_url': 'https://unsplash.com/@repponen',
+    #     'subtitle_color': '#fff'
+    # },
+    # {
+    #     'image': 'karsten-wurth.jpg',
+    #     'main_color': '#fff',
+    #     'main_title_color': '#fff',
+    #     'author': 'Karsten Würth',
+    #     'author_url': 'https://unsplash.com/@inf1783',
+    #     'subtitle_color': '#fff'
+    # },
+    # {
+    #     'image': 'nasa.jpg',
+    #     'main_color': '#000',
+    #     'main_title_color': '#333',
+    #     'author': 'NASA',
+    #     'author_url': 'https://unsplash.com/@nasa',
+    #     'subtitle_color': '#777'
+    # },
+    # {
+    #     'image': 'paulo-simoes-mendes.jpg',
+    #     'main_color': '#fff',
+    #     'main_title_color': '#fff',
+    #     'author': 'Paulo Simões Mendes',
+    #     'author_url': 'https://unsplash.com/@paulozono',
+    #     'subtitle_color': '#fff'
+    # },
+    # {
+    #     'image': 'nathan-anderson.jpg',
+    #     'main_color': '#fff',
+    #     'main_title_color': '#fff',
+    #     'author': 'PNathan Anderson',
+    #     'author_url': 'https://unsplash.com/@nathananderson',
+    #     'subtitle_color': '#fff'
+    # },
+    # {
+    #     'image': 'nathan-anderson2.jpg',
+    #     'main_color': '#fff',
+    #     'main_title_color': '#fff',
+    #     'author': 'PNathan Anderson',
+    #     'author_url': 'https://unsplash.com/@nathananderson',
+    #     'subtitle_color': '#fff'
+    # },
     {
-        'image': 'alex-siale.jpg',
+        'image': 'jumbotron.jpg',
         'main_color': '#fff',
         'main_title_color': '#fff',
-        'author': 'Alez Siale',
-        'author_url': 'https://unsplash.com/@alexsialecreative',
-        'subtitle_color': '#fff'
-    },
-    {
-        'image': 'anton-repponen.jpg',
-        'main_color': '#fff',
-        'main_title_color': '#fff',
-        'author': 'Anton Repponen',
-        'author_url': 'https://unsplash.com/@repponen',
-        'subtitle_color': '#fff'
-    },
-    {
-        'image': 'karsten-wurth.jpg',
-        'main_color': '#fff',
-        'main_title_color': '#fff',
-        'author': 'Karsten Würth',
-        'author_url': 'https://unsplash.com/@inf1783',
-        'subtitle_color': '#fff'
-    },
-    {
-        'image': 'nasa.jpg',
-        'main_color': '#000',
-        'main_title_color': '#333',
-        'author': 'NASA',
-        'author_url': 'https://unsplash.com/@nasa',
-        'subtitle_color': '#777'
-    },
-    {
-        'image': 'paulo-simoes-mendes.jpg',
-        'main_color': '#fff',
-        'main_title_color': '#fff',
-        'author': 'Paulo Simões Mendes',
-        'author_url': 'https://unsplash.com/@paulozono',
-        'subtitle_color': '#fff'
-    },
-    {
-        'image': 'nathan-anderson.jpg',
-        'main_color': '#fff',
-        'main_title_color': '#fff',
-        'author': 'PNathan Anderson',
-        'author_url': 'https://unsplash.com/@nathananderson',
-        'subtitle_color': '#fff'
-    },
-    {
-        'image': 'nathan-anderson2.jpg',
-        'main_color': '#fff',
-        'main_title_color': '#fff',
-        'author': 'PNathan Anderson',
-        'author_url': 'https://unsplash.com/@nathananderson',
+        'author': '',
+        'author_url': '',
         'subtitle_color': '#fff'
     },
 ]
